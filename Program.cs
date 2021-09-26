1a67e362e5c0981050acaeb877fc7da3719c93b9
MrDoggus <you@example.com>
2021-09-25 23:48:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DogChessEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Board
+    {
+        private byte[] board = new byte[64];
+    }
+}
